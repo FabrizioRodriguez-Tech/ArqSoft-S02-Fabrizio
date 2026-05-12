cec087a9734cc723f1e18fe9b28f538d9ac9fdaf
FabrizioRodriguez-Tech <alexander.rodriguez@tecdesoftware.edu.mx>
2026-05-12 12:58:49 -0600

    REFACTOR: separar en IRepositorioPalabras y PalabrasEnMemoria

---
+++ IRepositorioPalabra.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ahorcado
+{
+    internal class IRepositorioPalabra
+    {
+        namespace Ahorcado
+    {
+        public interface IRepositorioPalabra {
+            string ObtenerPalabraAleatoria();
+        }
+    }
+}
---
+++ PalabrasEnMemoria.cs
@@ -0,0 +1,24 @@
+﻿using Ahorcado.Ahorcado;
+using System;
+using System.Collections.Generic;
+
+namespace Ahorcado
+{
+    internal class PalabrasEnMemoria : IRepositorioPalabras
+    {
+        private readonly List<string> _palabras = new()
+        {
+            "arquitectura",
+            "interfaz",
+            "polimorfismo",
+            "encapsulamiento",
+            "herencia"
+        };
+
+        public string ObtenerPalabraAleatoria()
+        {
+            var random = new Random();
+            return _palabras[random.Next(_palabras.Count)];
+        }
+    }
+}
